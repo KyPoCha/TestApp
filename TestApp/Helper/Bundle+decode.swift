c8593d96757fc9d5dcc5d311933c8d23548e0dff
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2025-01-09 15:02:42 +0100

    Add resources and bundle decode for local app testing

---
+++ TestApp/Helper/Bundle+decode.swift
@@ -0,0 +1,30 @@
+//
+//  Bundle+decode.swift
+//  TestApp
+//
+//  Created by Kyrylo Polishchuk on 09.01.2025.
+//
+
+import UIKit
+
+import Foundation
+
+public extension Bundle {
+    func decode<T: Decodable>(_ type: T.Type, from file: String) -> T {
+        guard let url = self.url(forResource: "\(file)", withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+
+        let decoder = JSONDecoder()
+
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+
+        return loaded
+    }
+}
