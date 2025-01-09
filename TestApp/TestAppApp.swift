f0cdf5094f5fbb165e2f4d925236f3cf0f0ec760
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2025-01-09 13:10:26 +0100

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Kyrylo Polishchuk on 09.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
